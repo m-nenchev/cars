a81b826122035ab12a2c302939b2e40a1e9ef6ae
m-nenchev <milannn28@abv.bg>
2024-10-18 07:33:53 +0300

    AdjustedI  the header design and made it mobile responsive

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -4,61 +4,51 @@ import {UserContext} from '../../Context/Context'
 import {useContext, useEffect,useState} from 'react'
 import './Header.css'
 
-
 function Header() {
 const [email, setEmail] = useState('')
 const[ setUser]=useContext(UserContext)
-  useEffect(()=>{
-  
-   auth.onAuthStateChanged((user)=>{
-   //console.log(user);
-        if(!user){
-        return ;
+
+useEffect(()=>{
+  auth.onAuthStateChanged((user)=>{
+ //console.log(user);
+    if(!user){
+    return ;
+    }
+    setEmail(user.email);
+  })
+},[])
+return (
+  <header className="conteiner">
+   <nav className="navbar">
+    <input type="checkbox" id="sidebar-active" />
+    <label for="sidebar-active" className="open-sidebar-button">
+    <svg xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 -960 960 960" width="32"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"></path></svg>
+    </label>
+      <div className="navbar-1">
+      <label for="sidebar-active" className="close-sidebar-button">
+        <svg xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 -960 960 960" width="32"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"></path></svg>
+      </label>
+        <Link className="listitem"  to="/" >Home</Link>
+        <Link className="listitem" to="/forum" >Forum</Link>
+        <Link className="listitem" to="/about/image" >Image</Link>
+        <Link className="listitem" to="/cars" >Cars</Link>
+        {setUser
+        ?<span className="userName">UserName: {email}</span>
+        :<span className="userName"></span>
         }
-      
-        setEmail(user.email);
-       
-        
-      
-    })
-  
- 
-  },[])
-
-    return (
-        <header className="conteiner">
-         <nav className="navbar">
-            <section className="header">
-               <ul className="navbar-1">
-                  <li><Link className="listitem"  to="/" >Home</Link></li>
-                  <li><Link className="listitem" to="/forum" >Forum</Link></li>
-                  <li><Link className="listitem" to="/about/image" >Image</Link></li>
-                  <li><Link className="listitem" to="/cars" >Cars</Link></li>
-                  {setUser
-                  ?<span className="userName">UserName: {email}</span>
-                  :<span className="userName">UserName: </span>
-
-                }
-                     <div className="LoginDiv">
-                       {setUser
-                       ?  <span className="LoginButton">
-                        <li className="span-li"><Link className="listitem" to="/logOut">LogOut</Link></li>
-                       </span>
-                       :<span>
-                      <li className="span-li"><Link className="listitem" to="/login" >Login</Link></li>
-                       <li className="span-li"><Link className="listitem" to="/register" >Register</Link></li>
-                      
-                       </span>
-                      }
-                       </div>
-                </ul>
-            </section> 
-
-            <section className="navbar-anonymous">
-                
-            </section>
-            </nav>
-        </header>
-    );
+        {setUser
+        ?  <span className="LoginButton">
+         <Link className="listitem" to="/logOut">LogOut</Link>
+        </span>
+        :
+        <>
+        <Link className="listitem listitemLogin" to="/login" >Login</Link>
+        <Link className="listitem" to="/register" >Register</Link>
+        </>
+        }        
+      </div>
+    </nav>
+  </header>
+  );
 }
 export default Header;
